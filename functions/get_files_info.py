924a393a2b8d04f9e149a823781c9a870c7dc165
Faysal Hasan <faysalhasan950@gmail.com>
2026-05-31 15:21:06 +0600

    added get file list function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,15 +4,27 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
         working_dir_abs = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-        validate_dir = os.path.commonpath(working_dir_abs, target_dir)
+        validate_dir = os.path.commonpath([working_dir_abs, target_dir])
 
         if not validate_dir == working_dir_abs:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if not os.path.isdir(target_dir):
             return f'Error: "{directory}" is not a directory'
-        return f'Success: "{directory}" is within the working directory'
+
+        files = os.listdir(target_dir)
+
+        output = []
+        
+        for file in files:
+            is_dir = os.path.isdir(os.path.join(target_dir, file))
+            if not is_dir:
+                output.append(f"- {file}: file_size={os.path.getsize(os.path.join(target_dir, file))}, is_dir={is_dir}")
+            else:
+                output.append(f"- {file}: folder_size=unknown, is_dir={is_dir}")
+
+        return f"Result for {"current" if directory == "." else directory} directory: \n" + "\n".join(output)
 
     except Exception as e:
         return f"Error: An unexpected error occured: {str(e)}"
 
-get_files_info("calculator", "main.py")
+get_files_info("calculator")
