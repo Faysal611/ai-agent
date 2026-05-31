01ea6284976176ecb1a8ce90efed4383f8772f2e
Faysal Hasan <faysalhasan950@gmail.com>
2026-05-31 06:56:52 +0600

    added path validation

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+from google import genai
+import os
+import sys
+
 def main():
-    print("Hello from ai-agent!")
+    if len(sys.argv) < 2:
+        print("You need to provide a prompt!")
+        sys.exit(1)
 
+    verbose = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose = True
 
-if __name__ == "__main__":
-    main()
+    prompt = sys.argv[1]
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-3.1-flash-lite",
+        contents=prompt
+    )
+
+    if verbose:
+        print(response.text)
+        print(f"User Prompt: {prompt}")
+        print(f"Prompt token: {response.usage_metadata.prompt_token_count}")
+        print(f"Response token: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(response.text)
+main()
